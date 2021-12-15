e6d08fa624ff80e6e51d4c526d995d4f26283210
Philipp <poepsi39@gmail.com>
2021-12-15 09:48:01 +0100

    Implement optional CPU consuming busy waiting

--- main.go
+++ main.go
@@ -22,49 +22,89 @@ const defaultLognormMean = 25
 const defaultLognormStdev = 15
 
 func static(c *gin.Context) {
-	params := struct{
-		Time float64 `form:"time"`
+	params := struct {
+		Time   float64 `form:"time"`
+		Cores  int     `form:"cores"`
+		Stress string  `form:"stress"`
 	}{
-		Time: defaultStatic,
+		Time:   defaultStatic,
+		Cores:  1,
+		Stress: "no",
 	}
 	c.ShouldBindQuery(&params)
-	sleepAndRespond(c, time.Duration(params.Time) * time.Millisecond)
+	stress := true
+	if params.Stress == "no" {
+		stress = false
+	}
+	sleepAndRespond(c, time.Duration(params.Time)*time.Millisecond, params.Cores, stress)
 }
 
 func uniform(c *gin.Context) {
 	params := struct {
-		From int64 `form:"from"`
-		To int64 `form:"to"`
+		From   int64  `form:"from"`
+		To     int64  `form:"to"`
+		Cores  int    `form:"cores"`
+		Stress string `form:"stress"`
 	}{
-		From: defaultUniformFrom,
-		To: defaultUniformTo,
+		From:   defaultUniformFrom,
+		To:     defaultUniformTo,
+		Cores:  1,
+		Stress: "no",
 	}
 	c.BindQuery(&params)
 	delay := uniformRand(params.From, params.To)
-	sleepAndRespond(c, time.Millisecond * time.Duration(delay))
+	stress := false
+	if params.Stress == "no" {
+		stress = true
+	}
+	sleepAndRespond(c, time.Millisecond*time.Duration(delay), params.Cores, stress)
 }
 
 func lognorm(c *gin.Context) {
 	params := struct {
-		Mean float64 `form:"mean"`
-		Stdev float64 `form:"stdev"`
+		Mean   float64 `form:"mean"`
+		Stdev  float64 `form:"stdev"`
+		Cores  int     `form:"cores"`
+		Stress string  `form:"stress"`
 	}{
-		Mean:  defaultLognormMean,
-		Stdev: defaultLognormStdev,
+		Mean:   defaultLognormMean,
+		Stdev:  defaultLognormStdev,
+		Cores:  1,
+		Stress: "no",
 	}
 	c.BindQuery(&params)
 	delay := lognormalRand(params.Mean, params.Stdev)
-	sleepAndRespond(c, time.Millisecond * time.Duration(delay))
+	stress := false
+	if params.Stress == "no" {
+		stress = true
+	}
+	sleepAndRespond(c, time.Millisecond*time.Duration(delay), params.Cores, stress)
 }
 
-func sleepAndRespond(c *gin.Context, sleepTime time.Duration) {
+func busySleep(done chan int) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+		}
+	}
+}
+
+func sleepAndRespond(c *gin.Context, sleepTime time.Duration, cores int, stress bool) {
+	done := make(chan int)
+	if stress == true {
+		for i := 0; i < cores; i++ {
+			go busySleep(done)
+		}
+	}
 	time.Sleep(sleepTime)
+	close(done)
 	c.JSON(http.StatusOK, gin.H{
-		"first": 1234.1234,
+		"first":  1234.1234,
 		"second": 1234.1234,
-		"third": 1234.1234,
+		"third":  1234.1234,
 		"fourth": 1234.1234,
-		"fifth": 1234.1234,
+		"fifth":  1234.1234,
 	})
 }
-
