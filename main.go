01e2c73c29c11232e8a266447cadcc3e9a481e23
Philipp <poepsi39@gmail.com>
2021-12-29 19:47:44 +0100

    Set header for end timestamp

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func sleepAndRespond(c *gin.Context, sleepTime time.Duration, cores int) {
 	time.Sleep(sleepTime)
 	close(done)
 	end := time.Now()
-	c.Header("X-End", fmt.Sprintf("%.7f", float64(end.UnixNano())/float64(1000000000)))
+	c.Writer.Header().Set("X-End", fmt.Sprintf("%.7f", float64(end.UnixNano())/float64(1000000000)))
 	c.JSON(http.StatusOK, gin.H{
 		"first":  1234.1234,
 		"second": 1234.1234,
